56c7fb73a9e28c12ca5e40dd5fa2fbda3687fd9e
Alice Rees <Alice.Rees@zonal.co.uk>
2017-12-27 16:29:13 +0000

    commit

---
+++ Bank/Bank/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿namespace Bank
+{
+    internal class BankAccount
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public string Type { get; set; }
+
+        public BankAccount(string name, int amount, string type)
+        {
+            this.Name = name;
+            this.Amount = amount;
+            this.Type = type;
+        }
+
+        public void Deposit(int depositAmount)
+        {
+            this.Amount += depositAmount;
+        }
+
+        public void PayFee()
+        {
+            if (this.Type.Equals("business"))
+            {
+                this.Amount -= 50;
+            }
+            else if (this.Type.Equals("personal"))
+            {
+                this.Amount -= 10;
+            }
+        }
+    }
+}
---
+++ Bank/Bank/BankTest.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+
+namespace Bank
+{
+    [TestFixture]
+    public class BankTest
+    {
+        [Test]
+        public void TestName()
+        {
+            BankAccount account = new BankAccount("Alice", 500, "personal");
+            Assert.AreEqual("Alice", account.Name);
+        }
+
+        [Test]
+        public void TestAmount()
+        {
+            BankAccount account = new BankAccount("Alice", 500, "personal");
+            Assert.AreEqual(500, account.Amount);
+        }
+
+        [Test]
+        public void TestType()
+        {
+            BankAccount account = new BankAccount("Alice", 500, "personal");
+            Assert.AreEqual("personal", account.Type);
+        }
+
+        [Test]
+        public void TestSetName()
+        {
+            BankAccount account = new BankAccount("Alice", 500, "personal");
+            account.Name = "Matt";
+            Assert.AreEqual("Matt", account.Name);
+        }
+
+        [Test]
+        public void TestSetAmount()
+        {
+            BankAccount account = new BankAccount("Alice", 500, "personal");
+            account.Amount = 400;
+            Assert.AreEqual(400, account.Amount);
+        }
+
+        [Test]
+        public void TestSetType()
+        {
+            BankAccount account = new BankAccount("Alice", 500, "personal");
+            account.Type = "business";
+            Assert.AreEqual("business", account.Type);
+        }
+
+        [Test]
+        public void TestDeposit()
+        {
+            BankAccount account = new BankAccount("Alice", 500, "personal");
+            account.Deposit(50);
+            Assert.AreEqual(550, account.Amount);
+        }
+
+        [Test]
+        public void TestPayFeeBusiness()
+        {
+            BankAccount account = new BankAccount("Alice", 500, "business");
+            account.PayFee();
+            Assert.AreEqual(450, account.Amount);
+        }
+
+        [Test]
+        public void TestPayFeePersonal()
+        {
+            BankAccount account = new BankAccount("Alice", 500, "personal");
+            account.PayFee();
+            Assert.AreEqual(490, account.Amount);
+        }
+    }
+}
